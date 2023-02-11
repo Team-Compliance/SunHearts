c5a966b36a9774f400fdc7e8accb3fe3ff5cd088
BrakeDude <lockbrake@gmail.com>
2023-02-12 00:42:09 +0300

    Tweaks

--- lua/SunClot.lua
+++ lua/SunClot.lua
@@ -20,35 +20,6 @@ function mod:StaticHP(clot)
 end
 mod:AddCallback(ModCallbacks.MC_POST_FAMILIAR_RENDER, mod.StaticHP, 238)
 
---SPAWNING
---t eve's ability
-function mod:SunClotSpawn(baby)
-	local player = baby.Player
-	if baby.SubType == 30 then
-		if ComplianceSun.GetSunHeartsNum(player) % 2 == 0 then
-			SFXManager():Play(Isaac.GetSoundIdByName("SunHeartBreak"),1,0)
-			local shatterSPR = Isaac.Spawn(EntityType.ENTITY_EFFECT, 904, 0, player.Position + Vector(0, 1), Vector.Zero, nil):ToEffect():GetSprite()
-			shatterSPR.PlaybackSpeed = 2
-		end
-		local clot
-		for _, s_clot in ipairs(Isaac.FindByType(3,238,30)) do
-			s_clot = s_clot:ToFamiliar()
-			if GetPtrHash(s_clot.Player) == GetPtrHash(player) and GetPtrHash(baby) ~= GetPtrHash(s_clot) then
-				clot = s_clot
-				break
-			end
-		end
-		if clot ~= nil then
-			local clotData = clot:GetData()
-			clotData.TC_HP = clotData.TC_HP + 1
-			local SunEffect = Isaac.Spawn(EntityType.ENTITY_EFFECT, 903, 0, clot.Position + Vector(0, 1), Vector.Zero, nil):ToEffect()
-			SunEffect:GetSprite().Offset = Vector(0, -10)
-			baby:Remove()
-		end
-	end
-end
-mod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, mod.SunClotSpawn, 238)
-
 function mod:SunClotClear(rng, pos)
 	if #Isaac.FindByType(3,238,30) > 0 then	
 		for i=0, game:GetNumPlayers()-1 do
